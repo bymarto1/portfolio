b4390af72f1fab97076b93ce90e3c766680730ff
bymarto1 <62573271+bymarto1@users.noreply.github.com>
2023-08-03 18:10:47 +0200

    navbar and main

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import HeroSection from '../components/HeroSection'
+export default function Home() {
+  return (    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+  <HeroSection />
+
+</main>
+  )
+}
