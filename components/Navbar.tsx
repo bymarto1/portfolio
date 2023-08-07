188df1f4959994ecd3b674e62216200d3f96ddd0
bymarto1 <62573271+bymarto1@users.noreply.github.com>
2023-08-07 23:48:11 +0200

    navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import { useState  } from "react"
+import { useState } from "react"
 import { Link } from "react-scroll/modules";
 import { usePathname } from "next/navigation"
 import { useTheme } from "next-themes";
@@ -35,7 +35,7 @@ export default function Navbar() {
   const [navbar, setNavbar] = useState(false)
 
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20  min-height: 49.75rem  ">
+    <header className="relative w-full mx-auto  px-4 sm:px-20  min-height: 49.75rem  ">
       <div className="flex justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -45,70 +45,59 @@ export default function Navbar() {
                 <h3 className="text-xl text-gray-700">Software Engineer</h3>
               </div>
             </Link>
-            <div className="md:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
-              </button>
-            </div>
+              <div className="absolute top-4 right-4 md:hidden">
+            <button
+              className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+              onClick={() => setNavbar(!navbar)}
+            >
+              {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+            </button>
+          </div>
           </div>
         </div>
 
         <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? "block" : "hidden"
-            }`}
-          >
-            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-              {NAV_ITEMS.map((item, idx) => {
-                return (
-                  <Link
-                    key={idx}
-                    to={item.page}
-                    className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
-                    }
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-100}
-                    duration={500}
-                    onClick={() => setNavbar(!navbar)}
-                  >
-                    {item.label}
-                  </Link>
-                )
-              })}
-              <a href="https://github.com/bymarto1" target="_blank" rel="noopener noreferrer">
+        <div className={`absolute top-16 right-4 md:hidden ${navbar ? "block" : "hidden"}`}>
+          <div className="flex flex-col items-start space-y-8">
+              {NAV_ITEMS.map((item, idx) => (
+                <Link
+                  key={idx}
+                  to={item.page}
+                  className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100"
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {item.label}
+                </Link>
+              ))}
+
+              <a href="https://github.com/bymarto1" target="_blank" rel="noopener noreferrer" className="p-2">
                 <FaGithub size={25} />
               </a>
 
-              <a href="https://www.linkedin.com/in/pau-martori" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.linkedin.com/in/pau-martori" target="_blank" rel="noopener noreferrer" className="p-2">
                 <FaLinkedin size={25} />
               </a>
 
-              {currentTheme === "dark" ? (
-                <button
-                  onClick={() => setTheme("light")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
-                  <RiSunLine size={25} color="black" />
-                </button>
-              ) : (
-                <button
-                  onClick={() => setTheme("dark")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
-                  <RiMoonFill size={25} />
-                </button>
-              )}
+              <div>
+                {currentTheme === "dark" ? (
+                  <button onClick={() => setTheme("light")} className="bg-slate-100 p-2 rounded-xl">
+                    <RiSunLine size={25} color="black" />
+                  </button>
+                ) : (
+                  <button onClick={() => setTheme("dark")} className="bg-slate-100 p-2 rounded-xl">
+                    <RiMoonFill size={25} />
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
     </header>
-)
+  )
 }
